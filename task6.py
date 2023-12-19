e5476fab98b5734ff1456b40c8072855577a064f
imranagic <94862639+imranagic@users.noreply.github.com>
2023-12-19 23:21:49 +0100

    Update task6.py

--- task6.py
+++ task6.py
@@ -42,11 +42,15 @@ if __name__ == '__main__':
         student_marks[name] = scores
     query_name = input()
 
+    if query_name in student_marks and len(student_marks[query_name]): 
+        print(f'{sum(student_marks[query_name])/len(student_marks[query_name]):.2f}')
+
+    """
     suma = 0
     
     for score in student_marks[query_name]: 
         suma += score
     suma /= len(student_marks[query_name])
     print(f'{suma:.2f}')
-    
+    """
     
